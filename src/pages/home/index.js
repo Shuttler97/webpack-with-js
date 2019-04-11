b128a2e749e1d78d03248173ced1e7ea08cad7cb
utkarsh <ukv2010@gmail.com>
2019-04-11 17:10:03 +0530

    text added with animation

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,17 @@
 import "../../assets/style/home.scss";
 
-$(document).ready(() => {
-    console.log("Home page loaded!!");
-    let welcomeTag = document.getElementById("welcome");
-    welcomeTag.textContent = "Welcome to conatuhghgjgukgks home page";
+// $(document).ready(() => {
+//     console.log("Home page loaded!!");
+//     let welcomeTag = document.getElementById("welcome");
+//     welcomeTag.textContent = "Welcome to conatuhghgjgukgks home page";
+// });
+$(function(){
+    $('.repeat').click(function(){
+        var classes =  $(this).parent().attr('class');
+        $(this).parent().attr('class', 'animate');
+        var indicator = $(this);
+        setTimeout(function(){
+            $(indicator).parent().addClass(classes);
+        }, 20);
+    });
 });
