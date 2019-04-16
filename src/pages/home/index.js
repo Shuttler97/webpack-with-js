f59b8019d98bae47c24b77d883ed7e7eeabe62a8
sayashchaudhary <naimishverma50@gmail.com>
2019-04-16 17:29:22 +0530

    navbar added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -242,3 +242,14 @@ export default {
 //     });
 // });
 
+$(function(){
+    var navbar = $('.navbar');
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() <= 40){
+            navbar.removeClass('navbar-scroll');
+        } else {
+            navbar.addClass('navbar-scroll');
+        }
+    });
+});
