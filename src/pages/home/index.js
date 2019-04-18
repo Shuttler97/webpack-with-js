2a558b06836cbf76deda423acb2258ca7f86b228
sayashchaudhary <naimishverma50@gmail.com>
2019-04-18 15:01:56 +0530

    gallery background

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -270,8 +270,14 @@ $(document).ready(function(){
 
     //Click event to scroll to top
     $('.scrollToTop').click(function(){
-        $('html, body').animate({scrollTop : 0},1000);
+        $('html, body').animate({scrollTop : 0},1500);
         return false;
     });
 
 });
+
+// $(window).on('load', function() { // makes sure the whole site is loaded
+//     $('#status').fadeOut(); // will first fade out the loading animation
+//     $('#preloader').delay(1500).fadeOut('slow'); // will fade out the white DIV that covers the website.
+//     $('body').delay(1500).css({'overflow':'visible'});
+// })
